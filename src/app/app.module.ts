35a73f9726277d132dadf71a0ce0655082b79550
Mike Josef Wilhelm <mwilhelm@internal.convexis.de>
2019-12-05 14:21:55 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TreeModule } from 'angular-tree-component';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { TreeViewComponent } from './tree-view/tree-view.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeViewComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeModule.forRoot(),
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tree-view/tree-view.component.ts
@@ -0,0 +1,89 @@
+import { Component, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import convertedData from '../../assets/convertetTypeHierarchy.json';
+import { TreeNode, TreeModel, ITreeOptions } from 'angular-tree-component';
+
+@Component({
+  selector: 'app-tree-view',
+  templateUrl: './tree-view.component.html',
+  styleUrls: ['./tree-view.component.css']
+})
+export class TreeViewComponent implements AfterViewInit {
+  constructor() { }
+
+
+  nodes = [convertedData];
+
+  options: ITreeOptions = {
+
+    useVirtualScroll: true,
+    nodeHeight: 22
+  };
+
+  @ViewChild('tree', { static: true }) tree;
+
+
+  ngAfterViewInit() {
+    const firstNode = this.tree.treeModel.roots[0]
+    firstNode.expand();
+  }
+
+
+  public setExpandedNodes(expandedNodeIds: any) {
+    this.tree.treeModel.expandedNodeIds = expandedNodeIds;
+    // this.refresh();
+  }
+  public collapseAll() {
+    let expandedNodeIds: any = {};
+    this.setExpandedNodes(expandedNodeIds);
+    this.ngAfterViewInit();
+  }
+
+  public expandAll() {
+    let expandedNodeIds: any = {};
+    for (let node of this.tree.treeModel.roots) {
+      expandedNodeIds = this.updateNode(node, expandedNodeIds, true);
+    }
+    this.setExpandedNodes(expandedNodeIds);
+
+  }
+  private updateNode(node: TreeNode, expandedNodeIds: any, expand: boolean) {
+    let newExp = expandedNodeIds;
+    const children = node.children;
+    if (children) {
+      for (const child of children) {
+        newExp = this.updateNode(child, newExp, expand);
+      }
+    }
+    if (node.hasChildren) {
+      return Object.assign({}, newExp, { [node.id]: expand });
+    }
+    return newExp;
+  }
+  filterTree(value: string, treeModel: TreeModel) {
+
+    const foundResults: TreeNode[] = [];
+    treeModel.filterNodes((node: TreeNode) => {
+      const nodeName = node.data.name.toLowerCase();
+      const searchValue = value.toLowerCase();
+      const nodeFound = nodeName.includes(searchValue);
+      if (nodeFound && node.hasChildren) {
+
+        for (const child of node.children) {
+          if (child.hasChildren){
+            foundResults.push(child);
+          }
+        }
+        foundResults.push(node);
+
+      }
+      return nodeFound;
+    });
+    foundResults.forEach((item) => {
+      item.children.forEach((child) => {
+        child.show();
+        child.ensureVisible();
+      });
+    });
+  }
+
+}
